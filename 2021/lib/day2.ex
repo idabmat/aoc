e3e9840785b9b132a3f86b49407ca8cb2c4984ca
Igor de Alcântara Barroso <igor@talimhq.com>
2021-12-04 19:05:17 +0000

    2021 2B

--- 2021/lib/day2.ex
+++ 2021/lib/day2.ex
@@ -4,26 +4,43 @@ defmodule Day2 do
     input_file: "lib/day2/input.txt"
   ]
 
-  def execute(%{input_file: input_file}) do
+  def execute(%{input_file: input_file, mode: mode}) do
     input_file
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> move()
+    |> move(mode)
     |> present()
   end
 
-  defp move(moves) do
-    initial = [horizontal: 0, depth: 0]
-    Enum.reduce(moves, initial, fn move, [horizontal: horizontal, depth: depth] ->
+  defp move(moves, :gold) do
+    initial = %{horizontal: 0, depth: 0, aim: 0}
+    Enum.reduce(moves, initial, fn move, %{horizontal: horizontal, depth: depth, aim: aim} ->
       case move do
-        "forward " <> forward -> [horizontal: horizontal + String.to_integer(forward), depth: depth]
-        "down " <> down -> [horizontal: horizontal, depth: depth + String.to_integer(down)]
-        "up " <> up -> [horizontal: horizontal, depth: depth - String.to_integer(up)]
+        "forward " <> forward ->
+          %{horizontal: horizontal + String.to_integer(forward), depth: depth + aim * String.to_integer(forward), aim: aim}
+        "down " <> down ->
+          %{horizontal: horizontal, depth: depth, aim: aim + String.to_integer(down)}
+        "up " <> up ->
+          %{horizontal: horizontal, depth: depth, aim: aim - String.to_integer(up)}
       end
     end)
   end
 
-  defp present(horizontal: horizontal, depth: depth), do: horizontal * depth
+  defp move(moves, _) do
+    initial = %{horizontal: 0, depth: 0}
+    Enum.reduce(moves, initial, fn move, %{horizontal: horizontal, depth: depth} ->
+      case move do
+        "forward " <> forward ->
+          %{horizontal: horizontal + String.to_integer(forward), depth: depth}
+        "down " <> down ->
+          %{horizontal: horizontal, depth: depth + String.to_integer(down)}
+        "up " <> up ->
+          %{horizontal: horizontal, depth: depth - String.to_integer(up)}
+      end
+    end)
+  end
+
+  defp present(%{horizontal: horizontal, depth: depth}), do: horizontal * depth
 
   defimpl Aoc do
     defdelegate execute(struct), to: Day2
--- 2021/test/day2_test.exs
+++ 2021/test/day2_test.exs
@@ -5,4 +5,9 @@ defmodule Day2Test do
     day = %Day2{input_file: "test/day2/input.txt"}
     assert Aoc.execute(day) == 150
   end
+
+  test "calculates new sub position with aim" do
+    day = %Day2{input_file: "test/day2/input.txt", mode: :gold}
+    assert Aoc.execute(day) == 900
+  end
 end

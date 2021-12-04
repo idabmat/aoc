ee342f2240d8e65edb50263993aa8dc95ead30ad
Igor de Alcântara Barroso <igor@talimhq.com>
2021-12-04 20:54:35 +0000

    2021 3B

--- 2021/lib/day3.ex
+++ 2021/lib/day3.ex
@@ -8,22 +8,49 @@ defmodule Day3 do
     input_file
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> Stream.map(&String.codepoints/1)
+    |> Enum.map(&String.codepoints/1)
     |> read_diagnostic(mode)
   end
 
-  defp read_diagnostic(_diagnostic, :gold) do
-    calculate_life_support_rating(%{o2: "10111", co2: "01010"})
+  defp read_diagnostic(diagnostic, :gold) do
+    diagnostic
+    |> extract_o2_and_co2_rating()
+    |> calculate_life_support_rating()
   end
 
   defp read_diagnostic(diagnostic, _) do
     diagnostic
-    |> Stream.zip()
-    |> Stream.map(&Tuple.to_list/1)
-    |> Stream.map(&Enum.frequencies/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.frequencies/1)
     |> extract_gamma_and_epsilon_rate()
     |> calculate_power_consumption()
   end
+
+  defp extract_o2_and_co2_rating(diagnostics) do
+    %{
+      o2: filter_ratings(diagnostics, 0, :o2),
+      co2: filter_ratings(diagnostics, 0, :co2)
+    }
+  end
+
+  defp filter_ratings([h], _, _), do: Enum.join(h)
+  defp filter_ratings(diagnostics, counter, rating) do
+    selected_bit =
+      diagnostics
+      |> Enum.map(&Enum.at(&1, counter))
+      |> Enum.frequencies
+      |> bit_criteria(rating)
+    diagnostics
+    |> Enum.filter(fn diagnostic ->
+      Enum.at(diagnostic, counter) == selected_bit
+    end)
+    |> filter_ratings(counter + 1, rating)
+  end
+
+  defp bit_criteria(%{"0" => zeros, "1" => ones}, :o2) when zeros > ones, do: "0"
+  defp bit_criteria(%{"0" => zeros, "1" => ones}, :co2) when zeros <= ones, do: "0"
+  defp bit_criteria(_, _), do: "1"
 
   defp extract_gamma_and_epsilon_rate(frequencies) do
     initial = %{gamma: "", epsilon: ""}
